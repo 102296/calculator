2f07d3378a0899e7a8204e938c09ed7e435e7429
joan jemutai <joan.jemutai@strathmore.edu>
2018-01-16 17:56:04 +0300

    add

---
+++ src/Calc.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class Calc {
+	public static void main(String[] args) {
+		 Main a = new Main();
+	      a.setSize(400,280);
+	       a.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	       a.setVisible(true);
+	       a.setResizable(false);
+	       
+	}
+
+}
+
